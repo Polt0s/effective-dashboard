60e033b7af6804909e199f81dd6cabd82adfb829
Polt0s <poltos.pro@gmail.com>
2022-03-07 17:44:24 +0300

    Added data for component TopNav

---
+++ src/components/TopNav/data.ts
@@ -0,0 +1,50 @@
+export const profileMenuData = [
+  {
+      link: "/",
+      // icon: "bx bx-user",
+      content: "Profile"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-wallet-alt",
+      content: "My Wallet"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-cog",
+      content: "Settings"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-log-out-circle bx-rotate-180",
+      content: "Logout"
+  }
+];
+
+export const notificationData = [
+  {
+      link: "/",
+      // icon: "bx bx-error",
+      content: "Curabitur id eros quis nunc suscipit blandit"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-package",
+      content: "Duis malesuada justo eu sapien elementum, in semper diam posuere"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-cart",
+      content: "Donec at nisi sit amet tortor commodo porttitor pretium a erat"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-error",
+      content: "In gravida mauris et nisi"
+  },
+  {
+      link: "/",
+      // icon: "bx bx-cart",
+      content: "Curabitur id eros quis nunc suscipit blandit"
+  }
+]
