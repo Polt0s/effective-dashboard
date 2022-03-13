307d36e809230514adbbf5378eaab5eeb2f6fecf
Polt0s <poltos.pro@gmail.com>
2022-03-13 23:49:46 +0300

    Added custom hooks useClickOutside

---
+++ hooks/useClickOutside.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const useClickOutside = (className: string, func: () => void) => {
+  const clickOutside = React.useCallback((event: MouseEvent) => {
+    const target = event.target as Element;
+    if (target && target.className === className) {
+      func();
+    }
+  }, [className, func]);
+
+  React.useEffect(() => {
+    document.addEventListener('click', clickOutside);
+
+    return () => {
+      document.removeEventListener('click', clickOutside);
+    }
+  }, [clickOutside]);
+};
