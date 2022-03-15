bfa10334d133faf6e0ae3f08cff57727ae37a6a1
Polt0s <poltos.pro@gmail.com>
2022-03-15 23:38:56 +0300

    Added config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
