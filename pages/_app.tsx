2015c2ccdf0b520bb011b6501c19097388b5c283
Polt0s <poltos.pro@gmail.com>
2022-03-22 23:36:53 +0300

    Update component _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { RecoilRoot } from 'recoil';
 
 import { Sidebar, TopNav } from 'components';
 
@@ -6,20 +8,36 @@ import 'styles/globals.css';
 import 'styles/grid.css';
 import 'styles/index.css';
 import 'styles/theme.css';
+import 'styles/config.css';
 
 const Application = ({ Component, pageProps }: AppProps) => {
+    const router = useRouter();
+    const listAuthorization = ['/login'];
+
   return (
-    <main className="grid-root">
-        <div className="layout">
-          <Sidebar />
-          <div className="layout__content">
-            <TopNav />
-            <div className="layout__content-main">
-              <Component {...pageProps} />
-            </div>
-          </div>
-        </div>
-    </main>
+    <RecoilRoot>
+        {listAuthorization.includes(router.pathname) ? (
+            <main>
+                <div className="layout">
+                    <Component {...pageProps} />
+                </div>
+            </main>
+        ) : (
+            <main id="root">
+                <div className="grid-root">
+                    <div className="layout">
+                        <Sidebar />
+                        <div className="layout__content">
+                        <TopNav />
+                        <div className="layout__content-main">
+                            <Component {...pageProps} />
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        )}
+    </RecoilRoot>
   );
 }
 
