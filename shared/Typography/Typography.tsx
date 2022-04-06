dc036f22984ac3ea26b58eaef4f0740666e29a6d
Polt0s <poltos.pro@gmail.com>
2022-04-06 23:04:51 +0300

    Update styles components

--- shared/Typography/Typography.tsx
+++ shared/Typography/Typography.tsx
@@ -21,5 +21,5 @@ export const Typography = ({ variant, children, style, align, className, size }:
   children
 );
 
-type TVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+type TVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'caption';
 type TTextAlign = 'center' | 'justify' | 'left' | 'right' | 'auto' | 'inherit' | 'start' | 'end';
