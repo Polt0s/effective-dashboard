dca2f3dcc925930f31f52e2d8d3ffa14ea7d2e34
Polt0s <poltos.pro@gmail.com>
2022-04-10 21:10:29 +0300

    Update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
     "./shared/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '640px',
+      // => @media (min-width: 640px) { ... }
+      'tablet': '1024px',
+      'desktop': '1280px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: {
+      fontSize: {
+        sm: ['14px', '20px'], // font-size/line-height
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '33px'],
+      },
+      spacing: {},
+      borderRadius: {}
+    },
   },
   plugins: [],
 }
